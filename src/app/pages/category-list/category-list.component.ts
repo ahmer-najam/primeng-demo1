a1b0c89596582830d8d99acae520e92afd2d9baf
Ahmer <ahmer-najam>
2023-04-05 13:56:52 +0500

    2023.04.05

--- src/app/pages/category-list/category-list.component.ts
+++ src/app/pages/category-list/category-list.component.ts
@@ -5,6 +5,7 @@ import { CategoryService } from 'src/app/shared/category.service';
 import * as _ from 'lodash';
 import { MessageService } from 'primeng/api';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-category-list',
@@ -17,6 +18,18 @@ export class CategoryListComponent implements OnInit {
   selectedRow: Category;
   isLoading = false;
 
+  toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    animation: true,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
   constructor(
     private service: CategoryService,
     private messageService: MessageService,
@@ -61,6 +74,8 @@ export class CategoryListComponent implements OnInit {
   }
 
   receiveData(data: Category) {
+    if (this.isCategoryExists(data)) return;
+
     if (data.$key != undefined) {
       this.categories = this.categories.filter((item) => {
         return item.$key != data.$key;
@@ -80,8 +95,36 @@ export class CategoryListComponent implements OnInit {
       res = this.service.update(data.id, data);
     }
 
-    //this.categories = [data, ...this.categories];
+    this.toast.fire({
+      icon: 'success',
+      title: 'Record submitted',
+    });
+  }
 
-    this.toastr.success('Record Submitted', 'Database');
+  isCategoryExists(data): boolean {
+    let record = this.categories.find((item) => {
+      return (
+        item.id != data.id &&
+        item.category.toLowerCase() == data.category.toLowerCase()
+      );
+    });
+
+    if (record) {
+      this.toast.fire({
+        icon: 'error',
+        title: 'Duplicate Record',
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  erroalert(message) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: message,
+    });
   }
 }
